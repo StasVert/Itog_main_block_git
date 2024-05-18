51db80a4b9fbdc092031a9814f557ec44c5bc1fb
Stanislav_Vertkin <s.vertkin@gmail.com>
2024-05-18 17:50:15 +0300

    added scheme and solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] IncomeArray = new string[8] {"1234", "1567", "-2", "computer science", "Hello", "2", "world", ":-)"};                    // Задание входного массива строк
+string[] OutputArray = new string[IncomeArray.Length];                                              // Выходной такой же длины
+void FillArray(string[] IncomeArray, string[] OutputArray)                                          // Метод заполнения выходного массиива значениями из входного при условии 
+{
+    int CounetrOutputArray = 0;
+    for (int CounetrIncomeArray = 0; CounetrIncomeArray < IncomeArray.Length; CounetrIncomeArray++)
+    {
+    if(IncomeArray[CounetrIncomeArray].Length <= 3)                                                 // Условие выбора подходящих значений (длина строки меньше 3 символов)
+        {
+        OutputArray[CounetrOutputArray] = IncomeArray[CounetrIncomeArray];
+        CounetrOutputArray++;
+        }
+    }
+}   
+void PrintArray(string[] array)                                                                     // Метод вывода на экран строкового массива 
+{
+    for (int CounetrIncomeArray = 0; CounetrIncomeArray < array.Length; CounetrIncomeArray++)
+    {
+        Console.Write($"{array[CounetrIncomeArray]} ");
+    }
+    Console.WriteLine();
+}
+
+FillArray(IncomeArray, OutputArray);
+PrintArray(OutputArray);
